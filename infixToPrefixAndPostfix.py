12ee82bf963ee41df1e729de267c39003aba8ca0
ashensn0w <118508166+ashensn0w@users.noreply.github.com>
2023-01-05 20:41:09 +0800

    initial

--- infixToPrefixAndPostfix.py
+++ infixToPrefixAndPostfix.py
@@ -1,70 +1,74 @@
 def infixToPrefixAndPostfix():
-    def inf2postfix(exp):
-        alph = set('abcdefghijklmnopqrstuvwxyz')
-        pr1 = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
-        stack = []
-        postfix = []
+    def Con1():
+        oprtr = {'+', '-', '*', '/', '(', ')', '^'}
+        pr1 = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}  # level of priority for operators
 
-        for ch in exp:
-            if ch == ' ':
-                continue
-            if ch in alph:
-                postfix.append(ch)
-            elif ch == '(':
-                stack.append(ch)
-            elif ch == ')':
-                while stack[-1] != '(':
-                    postfix.append(stack.pop())
-                stack.pop()
+        def inf2postfix(exp):
+            stack = []
+            postfix = []  # serves as postfix list
 
-            elif ch in pr1:
-                if len(stack) == 0:
-                    stack.append(ch)
-                elif stack[-1] != '(' and pr1[stack[-1]] >= pr1[ch]:
-                    while stack and stack[-1] != '(' and pr1[stack[-1]] >= pr1[ch]:
-                        postfix.append(stack.pop())
-                    stack.append(ch)
+            for ch in exp:
+                if ch not in oprtr:
+                    postfix.append(ch)  # appends in postfix list
+                elif ch == '(':
+                    stack.append(ch)  # appends in stack
+                elif ch == ')':
+                    while stack[-1] != '(':
+                        postfix.append(stack.pop())  # if the last element on stack is ), appends the character to postfix
+                        # list
+                    stack.pop()  # pops the current stack if between ()
 
+                elif ch in pr1:
+                    if len(stack) == 0:  # if the stack is empty, character is appended to stack
+                        stack.append(ch)
+                    elif stack[-1] != '(' and pr1[stack[-1]] >= pr1[ch]:  # as long as the last character on stack is not (
+                        while stack and stack[-1] != '(' and pr1[stack[-1]] >= pr1[ch]:  # and if preceding character on
+                            postfix.append(stack.pop())  # stack is >= to the last character, the preceding character
+                        stack.append(ch)  # is popped to the postfix list & succeeding character is appended on stack
+
+                    else:
+                        stack.append(ch)
+
+            while len(stack):  # appends remaining characters on stack to postfix list
+                postfix.append(stack.pop())
+
+            return postfix
+
+        def inf2prefix(exp):
+            e1 = ''  # initialization of empty string for prefix notation
+
+            for i in range(len(exp) - 1, -1, -1):  # iteration over the characters in reverse order
+                if exp[i] == '(':
+                    e1 += ')'  # appends closing parenthesis if character is (
+                elif exp[i] == ')':
+                    e1 += '('  # appends opening parenthesis if character is )
                 else:
-                    stack.append(ch)
+                    e1 += exp[i]  # otherwise appends character to e1
 
-        while len(stack):
-            postfix.append(stack.pop())
+            rev = inf2postfix(e1)  # initializes variable to call function inf2postfix() with e1 as argument
 
-        return postfix
+            return rev[::-1]  # returns a reversed version of inf2postfix(e1)
 
-
-    def inf2prefix(exp):
-        e1 = ''
-
-        for i in range(len(exp) - 1, -1, -1):
-            if exp[i] == '(':
-                e1 += ')'
-            elif exp[i] == ')':
-                e1 += '('
+        while True:
+            infix = input("\nEnter an Infix Expression: ")
+            operators = ["+", "-", "*", "/", "^"]
+            if infix.startswith(tuple(operators)):
+                print("This is already in prefix notation")
+            elif infix.endswith(tuple(operators)):
+                print("This is already in postfix notation")
             else:
-                e1 += exp[i]
+                prefix = inf2prefix(infix)  # Gets the prefix notation using inf2prefix () function
+                print("\nPrefix notation:", end=' ')
+                for ch in prefix:
+                    print(ch, end='')
+                print()
 
-        rev = inf2postfix(e1)
-
-        return rev[::-1]
-
-
-    infix = input("\nEnter an Infix Expression: ")
-
-    prefix = inf2prefix(infix)
-    print("\nPrefix notation:", end=' ')
-    for ch in prefix:
-        print(ch, end='')
-    print()
-
-    postfix = inf2postfix(infix)
-    print("Postfix notation:", end=' ')
-    for ch in postfix:
-        print(ch, end='')
-    print()
+                postfix = inf2postfix(infix)  # Gets the prefix notation using inf2prefix () function
+                print("Postfix notation:", end=' ')
+                for ch in postfix:
+                    print(ch, end='')
+                print()
+                break
 
 
-    def anpro(exp):
-        inf2postfix(exp)
-        inf2prefix(exp)
+    Con1()  # calls main function
